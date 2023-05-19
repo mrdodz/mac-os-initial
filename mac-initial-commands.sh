6f86a3929d4de2629239c4664853a456e6d51d9d
dodz <58638164+mrdodz@users.noreply.github.com>
2023-05-19 11:54:24 +0300

    Update mac-initial-commands.sh

--- mac-initial-commands.sh
+++ mac-initial-commands.sh
@@ -11,3 +11,8 @@ sudo scutil --set LocalHostName $HOSTNAME
 sudo scutil --set ComputerName $HOSTNAME
 dscacheutil -flushcache
 sudo /System/Library/CoreServices/ManagedClient.app/Contents/Resources/createmobileaccount -D -n $USERNAME
+sudo sysadminctl -adminUser ADMIN -adminPassword $BSTU_PASS -secureTokenOn $USERNAME -password $USER_PASS
+
+# (logout/login with user as an admin)
+# sudo sysadminctl -adminUser ncambanis -adminPassword *** -secureTokenOff bstu -password ***
+# sudo diskutil apfs updatePreboot 
